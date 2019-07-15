5f75d91e8801c8b47fbee08369a617f99869f241
Cameron Avelis <camavelis@Cam-As-MacBook-Pro.local>
2019-07-15 16:33:15 -0400

    inital

---
+++ src/main/java/com/cmavelis/store/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cmavelis.store.config.audit;
---
+++ src/main/java/com/cmavelis/store/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.cmavelis.store.web.rest.vm;
